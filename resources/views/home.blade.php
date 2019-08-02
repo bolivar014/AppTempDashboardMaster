756f0b3f69b4a8b33f7b21874aa778f11c01991d
unknown <bolivar014@gmail.com>
2019-08-02 12:49:53 -0500

    Integrando modificaciones Vista MasterPage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,127 @@
 @extends('layouts.app')
 
     @section('content')
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">Dashboard</div>
+        <div class="card">
+            <div class="card-header text-center">
+                <strong>Dashboard</strong>
+            </div>
+            <hr class="my-4">
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
+                    <h1 class="display-4">Bienvenidos a <strong>App-Dashboard</strong> </h1>
+                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr class="my-4">
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Leer Más...</a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-globe text-warning"></i>
                                 </div>
-                            @endif
-
-                            You are logged in!
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Capacity</p>
+                                    <p class="card-title">150GB
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-refresh"></i> Update Now
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-money-coins text-success"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Revenue</p>
+                                    <p class="card-title">$ 1,345
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-calendar-o"></i> Last day
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-vector text-danger"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Errors</p>
+                                    <p class="card-title">23
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-clock-o"></i> In the last hour
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-favourite-28 text-primary"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Followers</p>
+                                    <p class="card-title">+45K
+                                    <p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-refresh"></i> Update now
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,14 +38,11 @@
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link rel="stylesheet" href="{{ asset('assets/demo/demo.css') }}">
 
-    <!-- CDN Bootstrap-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- FIN - CDN Bootstrap-->
+
 </head>
 <body>
     <div class="wrapper ">
-        <div class="sidebar" data-color="white" data-active-color="danger">
+        <div class="sidebar" data-color="red" data-active-color="danger">
         <!--
             Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
         -->
@@ -55,8 +52,8 @@
                     <img src="../assets/img/logo-small.png">
                 </div>
             </a>
-            <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-            Creative Tim
+            <a href="#" class="simple-text logo-normal">
+            App-Dashboard
             <!-- <div class="logo-image-big">
                 <img src="../assets/img/logo-big.png">
             </div> -->
@@ -123,7 +120,7 @@
     </div>
     <div class="main-panel">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+    <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent" style="background: #2c2c2c!important;">
         <div class="container-fluid">
             <div class="navbar-wrapper">
                 <div class="navbar-toggle">
@@ -133,7 +130,7 @@
                         <span class="navbar-toggler-bar bar3"></span>
                     </button>
                 </div>
-                <a class="navbar-brand" href="#pablo">Paper Dashboard 2</a>
+                <a class="navbar-brand" href="#" style="color: white;">Bienvenidos</a>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -143,28 +140,28 @@
             <div class="collapse navbar-collapse justify-content-end" id="navigation">
                 <form>
                     <div class="input-group no-border">
-                        <input type="text" value="" class="form-control" placeholder="Search...">
+                        <input type="text" value="" class="form-control" id="formGroupExampleInput" name="formGroupExampleInput" placeholder="Buscar..." style="color: white;">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                                <i class="nc-icon nc-zoom-split"></i>
+                                <i class="nc-icon nc-zoom-split" style="color: white!important;"></i>
                             </div>
                         </div>
                     </div>
                 </form>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link btn-magnify" href="#pablo">
-                            <i class="nc-icon nc-layout-11"></i>
+                        <a class="nav-link btn-magnify" href="#">
+                            <i class="nc-icon nc-layout-11" style="color: white!important;"></i>
                             <p>
                                 <span class="d-lg-none d-md-block">Stats</span>
                             </p>
                         </a>
                     </li>
-                    <li class="nav-item btn-rotate dropdown">
+                    <li class="nav-item btn-rotate dropdown" style="color: white!important;">
                         <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="nc-icon nc-bell-55"></i>
+                            <i class="nc-icon nc-bell-55" style="color: white!important;"></i>
                             <p>
-                                <span class="d-lg-none d-md-block">Some Actions</span>
+                                <span class="d-lg-none d-md-block">Opciones</span>
                             </p>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
@@ -174,10 +171,10 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link btn-rotate" href="#pablo">
+                        <a class="nav-link btn-rotate" href="#">
                             <i class="nc-icon nc-settings-gear-65"></i>
                             <p>
-                                <span class="d-lg-none d-md-block">Account</span>
+                                <span class="d-lg-none d-md-block" style="color: white!important;">Account</span>
                             </p>
                         </a>
                     </li>
@@ -229,6 +226,7 @@
         </footer>
         </div>
     </div>
+
     <!--   Core JS Files   -->
     <script src="{{ asset('assets/js/core/jquery.min.js"></script>
     <script src="{{ asset('assets/js/core/popper.min.js"></script>
@@ -253,13 +251,6 @@
         });
     </script>
  
-    <!-- CDN JQuery -->
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <!-- FIN - CDN JQuery -->
-    <!-- CDN Bootstrap-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <!-- FIN - CDN Bootstrap-->
 
 </body>
 </html>
