827a7bb966e7cba0ffdec3df49f77cd61170b564
unknown <bolivar014@gmail.com>
2019-08-03 20:21:21 -0500

    asignando rutas Invitado y autenticado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,60 +61,82 @@
         </div>
         <div class="sidebar-wrapper">
             <ul class="nav">
-                <li class="active ">
-                    <a href="{{ url('/') }}">
-                        <i class="nc-icon nc-bank"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ url('/home') }}">
-                        <i class="nc-icon nc-badge"></i>
-                        <p>Home</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./icons.html">
-                        <i class="nc-icon nc-diamond"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./map.html">
-                        <i class="nc-icon nc-pin-3"></i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./notifications.html">
-                        <i class="nc-icon nc-bell-55"></i>
-                        <p>Notifications</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./user.html">
-                        <i class="nc-icon nc-single-02"></i>
-                        <p>User Profile</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./tables.html">
-                        <i class="nc-icon nc-tile-56"></i>
-                        <p>Table List</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./typography.html">
-                        <i class="nc-icon nc-caps-small"></i>
-                        <p>Typography</p>
-                    </a>
-                </li>
-                <li class="active-pro">
-                    <a href="./upgrade.html">
-                        <i class="nc-icon nc-spaceship"></i>
-                        <p>Upgrade to PRO</p>
-                    </a>
-                </li>
+                @guest
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('login') }}"> 
+                            <i class="nc-icon nc-bank"></i>
+                            {{ __('Ingresar') }} 
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">
+                            <i class="nc-icon nc-badge"></i>
+                            {{ __('Registrar') }}
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin/products">
+                            <i class="nc-icon nc-diamond"></i>
+                            Inicio
+                        </a>
+                    </li>
+
+                @else
+                    <li class="active ">
+                        <a href="{{ url('/') }}">
+                            <i class="nc-icon nc-bank"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/home') }}">
+                            <i class="nc-icon nc-badge"></i>
+                            <p>Home</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./icons.html">
+                            <i class="nc-icon nc-diamond"></i>
+                            <p>Icons</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./map.html">
+                            <i class="nc-icon nc-pin-3"></i>
+                            <p>Maps</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./notifications.html">
+                            <i class="nc-icon nc-bell-55"></i>
+                            <p>Notifications</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./user.html">
+                            <i class="nc-icon nc-single-02"></i>
+                            <p>User Profile</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./tables.html">
+                            <i class="nc-icon nc-tile-56"></i>
+                            <p>Table List</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./typography.html">
+                            <i class="nc-icon nc-caps-small"></i>
+                            <p>Typography</p>
+                        </a>
+                    </li>
+                    <li class="active-pro">
+                        <a href="./upgrade.html">
+                            <i class="nc-icon nc-spaceship"></i>
+                            <p>Upgrade to PRO</p>
+                        </a>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
